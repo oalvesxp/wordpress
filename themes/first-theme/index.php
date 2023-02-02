1aab7624674bdfc5d0b48ef189864eff924a3ce7
Osvaldo alves <oalvesxp@gmail.com>
2023-02-01 22:43:49 -0300

    adding the php functions on head.

--- themes/first-theme/index.php
+++ themes/first-theme/index.php
@@ -2,10 +2,12 @@
 <html>
     <head>
         <!-- Required meta tags -->
-        <meta charset="utf-8">
+        <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <?php wp_head(); ?>
     </head>
     <body>
-    
+            
     </body>
 </html>
