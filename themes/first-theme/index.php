5cb3ed416d66f637549799fd927927247ef11818
Osvaldo alves <oalvesxp@gmail.com>
2023-02-01 22:54:49 -0300

    adding posts on site body with wordpress functions

--- themes/first-theme/index.php
+++ themes/first-theme/index.php
@@ -11,6 +11,22 @@
         <h1><?php bloginfo('name'); ?></h1>
         <h2><?php bloginfo('description'); ?></h2>
 
+        <?php
+        if( have_posts() ) :
+            while( have_posts() ) :
+                the_post();
+                ?>
+
+                <h3><?php the_title(); ?></h3>
+                <div>
+                    <?php the_content(); ?>
+                </div>
+                
+                <?php
+            endwhile;
+        endif;
+        ?>
+
         <?php wp_footer(); ?>
     </body>
 </html>
